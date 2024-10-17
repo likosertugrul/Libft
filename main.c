77c91130f7d78d5abef5fe547b6bbf3fccd2a57e
Eruğrul Likos <ertugrullikos@Mac.home>
2024-10-17 15:53:16 +0300

    zaphood

--- main.c
+++ main.c
@@ -12,10 +12,15 @@ int main (void)
     char *surname;
     char *name2;
     char *surname2;
+    char *res;
+    char *son;
     name = "Ertugrul";
     name2 = "Ertugrul";
-    surname = strdup(name);
-    surname2 = ft_strdup(name2);
-    printf("%s, %s\n", name, surname);
-    printf("%s, %s\n", name2, surname2);
+    surname = "gr";
+    surname2 = "gr";
+
+    res = strnstr(name, surname, sizeof(name));
+    son = ft_strnstr(name2, surname2, sizeof(name2));
+    printf("%s, %s, %s \n", name, surname, res);
+    printf("%s, %s, %s \n", name2, surname2, son);
 }
